65ab75edddef72c750660db6cf27bd278c09ffc1
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-06-18 02:27:18 +0200

    Add webpack + babel
    In order to have a safe output for all the browsers, and usable as a module from Node

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path'),
+      package = require('./package.json');
+
+module.exports = {
+  entry: './src/',
+  resolve: {
+    extensions: [ '.es6', '.js' ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: package.name + '.js',
+    library: package.name,
+    libraryTarget: "umd2"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.es6$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            "presets": [
+              ["env", {
+                "targets": {
+                  "browsers": ["last 2 versions", "ie >= 11"]
+                }
+              }]
+            ],
+            "plugins": ["add-module-exports"]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    compress: true,
+    disableHostCheck: true,
+    host: "0.0.0.0",
+    port: 8080
+  }
+};
